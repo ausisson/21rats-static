3ac00f3ad0f084f982264b4b772e08c39f5588f0
ausisson <ausisson@gmail.com>
2025-07-28 19:54:36 -0500

    making nimrods arm move a little

---
+++ assets/mouse.js
@@ -0,0 +1,74 @@
+
+
+
+const ROTATEVAL = -27;
+const YVAL = 490;
+const XVAL = 545;
+const MAXROTATE = 50;
+const MINROTATE = -50;
+
+
+const handleMouse = (event) =>{
+	//console.log(event.clientX)
+}
+
+const handleClick = (event) => {
+
+	const mouseCoords = getMouseCoords(event);
+	const armCoords = getArmCoords();
+	
+	const angle = getAngle(mouseCoords);
+	console.log(angle)
+
+	animate(".arm",{
+		rotate:angle
+	})
+	//console.log("arm coords {x: " + armCoords.x + ", y: "+ armCoords.y + " }");
+	//console.log("click coords {x: " + mouseCoords.x + ", y: " + mouseCoords.y + " }");
+}
+
+const getMouseCoords = (event) =>{
+	return {x: Math.round(event.clientX), y:Math.round(event.clientY)}
+}
+
+const getArmCoords = () => {
+	const boundingRect = document.querySelector(".arm").getBoundingClientRect();
+	return {x:Math.round(boundingRect.x), y:Math.round(boundingRect.y)}
+}
+
+const getAngle = (coords) => {
+	const opp = coords.y - YVAL;
+	const adj = (coords.x - XVAL) < 0 ? coords.x-XVAL : -1;
+	console.log("opp" + opp)
+	console.log("adj" + adj)
+	let angle = (Math.atan2(opp,adj)*180/Math.PI) + 180;
+	if (angle > 90) {
+		angle = angle - 360;
+	}
+	angle = angle > MAXROTATE ? MAXROTATE : angle;
+	angle = angle < MINROTATE ? MINROTATE : angle
+	return angle - 27;
+
+}
+
+const inital = getArmCoords();
+document.addEventListener("mousemove", handleMouse);
+document.addEventListener("mousedown",handleClick);
+
+
+
+/*so, if i rotate the square -27 degrees, then the arm is in a straight line, 
+	so the arm has a slope of 0, as the y value is unchanging
+
+this basically means, that i need to find a way to calculate rotating the slope around the rotation
+
+at default, the slope of the line will be -.5
+so hypothetically, if the arm is rotated -54 degrees, the slope of the line will be .5?
+
+did the math (shittily, but still did it, and this works out)
+
+y value = 490
+
+
+
+*/
